a8bf3c78ba8ea634e2ad709da7b7748aa21d3b19
rainy6 <ryo167prog@gmail.com>
2023-09-24 17:43:48 +0900

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,7 +32,7 @@ const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
   return (
     <div className={styles.page}>
       <button onClick={handleClick} className={styles.button}>
-        one more cat!
+        one more cat!!
       </button>
       <div className={styles.frame}>
         {loading || <img src={imageUrl} className={styles.img} />}
